f9300b20f69f0a21623d68f79547deb83c02f118
nayeon <ice1011@naver.com>
2023-06-06 00:41:26 +0900

    억까당하기전..

--- src/main/java/com/neighborcharger/capstoneproject/controller/QRController.java
+++ src/main/java/com/neighborcharger/capstoneproject/controller/QRController.java
@@ -1,6 +1,8 @@
 package com.neighborcharger.capstoneproject.controller;
 
+import com.amazonaws.services.ec2.model.Reservation;
 import com.neighborcharger.capstoneproject.model.PrivateStation;
+import com.neighborcharger.capstoneproject.model.Reservation_info;
 import com.neighborcharger.capstoneproject.model.user.StationHardWare;
 import com.neighborcharger.capstoneproject.service.DB_Service;
 import com.neighborcharger.capstoneproject.service.Reservation_Service;
@@ -11,6 +13,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
 
 @Controller
 public class QRController {
@@ -40,10 +44,21 @@ public class QRController {
             model.addAttribute("RealRunTime", statHW.getRealRunTime());
             model.addAttribute("cost", statHW.getCost());
 
-        } else{
+        } else{ //충전 중이 아니라면 (처음 큐알을 찍는거라면) -> 이미 qrConnect에서 충전중이라고 디비에서는 바뀜
             System.out.println("###################################처음!!!");
-            model.addAttribute("RealStartTime", "몰라");
-            model.addAttribute("RealEndTime", "몰라");
+
+            // 선택된 개인충전소의 예약 목록에서 충전중인 것을 찾고, 그것의 endTime을 가져오자
+            Reservation_info findReservationInfo = new Reservation_info();
+            List<Reservation_info> reservationInfoList = privateStation.getReservations();
+            for(Reservation_info reservation_info : reservationInfoList){
+                if (reservation_info.getChecking() == "대기" || reservation_info.getChecking() == "수락"){
+                    findReservationInfo = reservation_info;
+                }
+            }
+
+            System.out.println(findReservationInfo.getStart_time() + "***** 이때 시작함");
+            model.addAttribute("end_time", findReservationInfo.getEnd_time());
+            System.out.println(findReservationInfo.getEnd_time() + "시간이 여깃어용");
         }
 
         return "QRCodeResponeVIew";
--- src/main/java/com/neighborcharger/capstoneproject/service/DB_Service.java
+++ src/main/java/com/neighborcharger/capstoneproject/service/DB_Service.java
@@ -125,4 +125,6 @@ public class DB_Service {
 
         return publicStationList;
     }
+
+
 }
